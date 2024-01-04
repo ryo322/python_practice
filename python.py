c904b22c503e1dee7df81bafff9a68e3a4d9d9e9
ryo322 <syaro12@icloud.com>
2024-01-04 14:22:14 +0900

    演算子

--- python.py
+++ python.py
@@ -1,20 +1,30 @@
-msg = ["勉強は", "大変だけど"]
-print("msg  ->", msg)
+s1 = input("aの値を入力してください＞")
+s2 = input("bの値を入力してください＞")
 
-msg.append("前に進もう")
-print("append ->", msg)
+print("s1 + s2 =", (s1 + s2))
 
-msg.remove("大変だけど")
-print("remove ->", msg)
+a = int(s1)
+b = int(s2)
 
-msg.insert(1,"楽しい")
-print("insert ->", msg)
+print("a=", a, ",b =", b)
+print("a + b =", (a + b))
+print("a - b =", (a - b))
+print("a * b =", (a * b))
+print("a ** b =", (a ** b))
+print("a / b =", (a / b))
+print("a // b =", (a // b))
+print("a % b =", (a % b))
 
-msg.clear()
-print("clear ->", msg)
+print("a >= 0 :", (a >= 0))
+print("(a >= 0) and (b >= 0) :", (a >= 0) and (b >= 0))
 
-#メソッド：特定の方で使える関数
-#append(値)：追加　　例）score.append(85)
-#insert(添字、値)：挿入　　例）score.insert(2,90)
-#remove(値)：削除　　例）score.remove(75)
-#clear()：全削除　　例）score.clear()
+
+#**は累乗で*は掛け算なので注意が必要
+#//は整数で計算を切り上げる。/は小数点まで計算
+
+#論理演算子
+#and 論理積
+#not 否定
+#or  論理和
+
+#input(文字列)：文字列を表示して入力された文字列を受け取る関数
