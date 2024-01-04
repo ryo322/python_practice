1c1f5e92685ce418cf4c02fbf906aba0c1819ee1
ryo322 <syaro12@icloud.com>
2024-01-04 15:53:02 +0900

    繰り返し処理

--- python.py
+++ python.py
@@ -1,15 +1,27 @@
-price_str = input("税抜き金額を入力してください")
-rate = 0.10
-price = int(price_str)
+num = [0, 1, 2, 3]
+for i in num:
+    print("i =", i)
 
-if price >= 5000:
-    discount = 500
-elif price >= 3000:
-    discount = 300
-else:
-    discount = 0
+for j in range(3):
+    print("j =", j)
 
-amount = int((price - discount) * (1 + rate))
+k = 0
+while k < 3:
+    print("k =", k)
+    k = k + 1
 
-print("値引金額：", discount, "円")
-print("税込金額：", amount, "円")
+sub_name = ["英語", "数学", "国語"]
+score = [90, 85, 80]
+sum_score = 0
+
+for i in range(3):
+    print(sub_name[i], ":", score[i], "点")
+    sum_score = sum_score + score[i]
+
+avg = sum_score / 3
+
+print("合計：", sum_score, "点")
+print("平均：", avg, "点")
+
+#range(終了値)：０から（終了値−１）までのイテラブルを得る関数
+#イテラブル：反復可能な複数のデータに集まり
