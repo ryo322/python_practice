389ab78f0a3802d15596c292dad4c6ddf1ab50d9
ryo322 <syaro12@icloud.com>
2024-01-14 15:11:22 +0900

    フィルター

--- python.py
+++ python.py
@@ -1,15 +1,13 @@
 numbers = [0, 3, 8, -4, 9, 1]
 
-numbers.insert(0, 5) #値を挿入するときは（何番目か, 挿入する値）
+def isEven(number):
+    if number % 2 == 0:
+        print(f'This number, {number} is even')
+        return True
+    else:
+        print(f'This number, {number} is odd')
+        return False
 
-print(numbers)
+list(filter(isEven, numbers))
 
-numbers.insert(-1, 4)
-
-print(numbers)
-
-numbers.remove(5) #値を削除する場合はremove
-
-print(numbers)
-
-numbers.pop(-3) #場所を指定して削除する場合はpop
+print(list)
