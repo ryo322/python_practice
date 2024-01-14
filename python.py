0a23043cbd2fd5151838829d07d6cb4918b54764
ryo322 <syaro12@icloud.com>
2024-01-14 15:16:01 +0900

    インデックス

--- python.py
+++ python.py
@@ -1,13 +1,3 @@
 numbers = [0, 3, 8, -4, 9, 1]
 
-def isEven(number):
-    if number % 2 == 0:
-        print(f'This number, {number} is even')
-        return True
-    else:
-        print(f'This number, {number} is odd')
-        return False
-
-list(filter(isEven, numbers))
-
-print(list)
+print(numbers.index(-4))
