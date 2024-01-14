63ee6c899c8505df624afe51fafd82175e5eeb6b
ryo322 <syaro12@icloud.com>
2024-01-14 14:55:15 +0900

    値を挿入

--- python.py
+++ python.py
@@ -1,9 +1,9 @@
 numbers = [0, 3, 8, -4, 9, 1]
 
-print(numbers[1])
+numbers.insert(0, 5) #値を挿入するときは（何番目か, 挿入する値）
 
-print(numbers[-1])
+print(numbers)
 
-numbers.append(3)
+numbers.insert(-1, 4)
 
 print(numbers)
