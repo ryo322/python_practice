a582cc217463c51db65516e4787ba5f72f00aad8
ryo322 <syaro12@icloud.com>
2024-01-15 12:40:33 +0900

    複数条件

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
-num = -1
-if num > 0:
-    print('正の値です')
-elif num == 0:
-    print('０です')
+a = -3
+if 0 <= a < 10 and a % 2 == 0:
+    print('一桁の偶数です')
+elif a < 0 and a % 2 == 1:
+    print('負の奇数です')
 else:
-    print('負の値です')
+    print('整数です')
