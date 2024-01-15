03ed4cb98019d369156a66ea108fda5ef00fa5fc
ryo322 <syaro12@icloud.com>
2024-01-15 12:28:13 +0900

    辞書の削除

--- python.py
+++ python.py
@@ -1,7 +1,15 @@
-numbers = [0, 3, 8, -4, 9, 1]
+dictionary = {
+    'A' : '田中',
+    'B' : '佐藤',
+    'C' : '男性',
+    'D' : '東京',
+    'E' : '海外旅行',
+}
 
-numbers.sort()
-print(numbers)
+print(dictionary)
 
-numbers.sort(reverse=True)
-print(numbers)
+dictionary.pop('A') #要素の削除は.pop(削除する項目)
+print(dictionary)
+
+dictionary.clear() #.clear()はすべての要素を削除
+print(dictionary)
