0e899023ad635518cab67ead04183d393e113bfc
ryo322 <syaro12@icloud.com>
2024-01-16 14:24:58 +0900

    複数のリストの同時処理

--- python.py
+++ python.py
@@ -1,6 +1,6 @@
-for i in range(10):
-    if i == 3:
-        continue
-    print(i)
+lasts = ['加藤', '佐藤', '田中']
+firsts = ['雄一', 'たくや', '太郎']
 
+for last, first in zip(lasts, firsts):
+    print(last+first)
 
