f5507f78cf136cfb7eddcc59437e76078d0cc9d0
ryo322 <syaro12@icloud.com>
2024-01-16 14:27:04 +0900

    要素とインデックスの取得

--- python.py
+++ python.py
@@ -1,6 +1,5 @@
 lasts = ['加藤', '佐藤', '田中']
-firsts = ['雄一', 'たくや', '太郎']
 
-for last, first in zip(lasts, firsts):
-    print(last+first)
+for i, last in enumerate(lasts):
+    print(f'{i}番目の{last}です。')
 
