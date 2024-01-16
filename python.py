56d7f5d19de4c82c8b5ce3363ac24e58505cfb91
ryo322 <syaro12@icloud.com>
2024-01-16 14:19:21 +0900

    リストの要素を表示

--- python.py
+++ python.py
@@ -1,7 +1,5 @@
-a = -3
-if 0 <= a < 10 and a % 2 == 0:
-    print('一桁の偶数です')
-elif a < 0 and a % 2 == 1:
-    print('負の奇数です')
-else:
-    print('整数です')
+names = ['john', 'kevin', 'louis']
+
+for name in names:
+    print(name)
+    
