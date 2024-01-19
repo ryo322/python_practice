12ae705af039625d60e33a5bb1e83b0c946f7c45
ryo322 <syaro12@icloud.com>
2024-01-19 10:34:23 +0900

    例外をスルー

--- python.py
+++ python.py
@@ -4,8 +4,8 @@ def divide(a, b):
     except ZeroDivisionError as e:
         print(e)
     except TypeError as e:
-        print(e)
+        pass
     finally:
         print('すべての処理が正常に終了しました。')
 
-print(divide(10, 5))
+print(divide(10, '5'))
