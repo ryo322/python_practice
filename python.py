4d37757b8a73480af27ba2193c259f78440191cc
ryo322 <syaro12@icloud.com>
2024-01-19 10:38:02 +0900

    クラス

--- python.py
+++ python.py
@@ -1,11 +1,10 @@
-def divide(a, b):
-    try:
-        print(f'計算結果： {a/b}')
-    except ZeroDivisionError as e:
-        print(e)
-    except TypeError as e:
-        pass
-    finally:
-        print('すべての処理が正常に終了しました。')
+class Person:
+    nationality = 'Japan'
 
-print(divide(10, '5'))
+    def say_hello(self):
+        print(f'こんにちは、私の国籍は{self.nationality}です')
+
+Person = Person()
+Person.nationality
+
+print(Person.say_hello())
