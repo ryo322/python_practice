04fd2dd0f089f701b2642888c0fec234cc7775bf
ryo322 <syaro12@icloud.com>
2024-01-19 10:42:18 +0900

    コンストラクタ

--- python.py
+++ python.py
@@ -1,10 +1,17 @@
 class Person:
     nationality = 'Japan'
 
+    def __init__(self, name):
+        self.name = name
+
     def say_hello(self):
         print(f'こんにちは、私の国籍は{self.nationality}です')
 
-Person = Person()
-Person.nationality
+    def say_my_name(self):
+        print(f'私の名前は{self.name}です。')
 
-print(Person.say_hello())
+A = Person('田中')
+A.nationality
+A.name
+
+print(A.say_my_name())
